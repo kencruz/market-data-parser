f2fa8fb710038a2c30f02b2f0e205bb6c7064eb6
Ken Cruz <ken@kencruz.ca>
2019-10-02 05:26:34 -0400

    added Quote struct placeholder

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use pcap_parser::traits::PcapReaderIterator;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use std::str;
 
 fn main() {
 
@@ -12,9 +13,23 @@ fn main() {
     let mut reader = LegacyPcapReader::new(65536, file).expect("PcapNGReader");
     loop {
         match reader.next() {
-            Ok((offset, _block)) => {
-                println!("got new block");
+            Ok((offset, block)) => {
+                //println!("got new block");
                 num_blocks += 1;
+                match block {
+                    PcapBlockOwned::LegacyHeader(_hdr) => {
+                        // save hdr.network (linktype)
+                    },
+                    PcapBlockOwned::Legacy(b) => {
+                        // use linktype to parse b.data()
+                        //let convert = match str::from_utf8(b.data) {
+                        //    Ok(v) => v,
+                        //    Err(e) => panic!("invalid utf-8 sequence: {}", e),
+                        //};
+                        println!("{}", b.ts_sec);
+                    },
+                    PcapBlockOwned::NG(_) => unreachable!(),
+                }
                 reader.consume(offset);
             },
             Err(PcapError::Eof) => break,
@@ -25,4 +40,31 @@ fn main() {
         }
     }
     println!("num_blocks: {}", num_blocks);
+}
+
+
+struct Quote {
+    pkt_time: u8,
+    accept_time: u8,
+    issue_code: u8,
+    bid_qty_1: u8,
+    bid_qty_2: u8,
+    bid_qty_3: u8,
+    bid_qty_4: u8,
+    bid_qty_5: u8,
+    bid_price_1: u8,
+    bid_price_2: u8,
+    bid_price_3: u8,
+    bid_price_4: u8,
+    bid_price_5: u8,
+    ask_qty_1: u8,
+    ask_qty_2: u8,
+    ask_qty_3: u8,
+    ask_qty_4: u8,
+    ask_qty_5: u8,
+    ask_price_1: u8,
+    ask_price_2: u8,
+    ask_price_3: u8,
+    ask_price_4: u8,
+    ask_price_5: u8,
 }
