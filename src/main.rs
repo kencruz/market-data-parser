fbf7f3e2fb32ab4291d97caa7c5ad9fd55224847
Ken Cruz <ken@kencruz.ca>
2019-10-04 04:13:18 -0400

    refactoring quote building, price/quantity formatting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use pcap_parser::traits::PcapReaderIterator;
 use pcap_parser::*;
 use std::fs::File;
-use std::io::Read;
 use std::path::Path;
 use std::str;
 
@@ -25,33 +24,37 @@ fn main() {
                             match str::from_utf8(&b.data[42..256]) {
                                 Ok(s) if s.starts_with("B6034") => {
                                     // build the quote here
+                                    let mut byte_string = s.chars();
+                                    // the chunk lengths of the iterator we're grouping together,
+                                    // negative numbers means skip
+                                    let slice_instructions = vec![
+                                        -5, 12, -12, 5, 7, 5, 7, 5, 7, 5, 7, 5, 7, -7, 5, 7, 5, 7,
+                                        5, 7, 5, 7, 5, 7, -50, 8,
+                                    ];
+                                    let arr = consume(&mut byte_string, slice_instructions);
                                     let pkt_time = b.ts_sec;
-                                    let mut byte_string = s.chars().skip(5);
-                                    let issue_code = consume_and_spit(&mut byte_string, 12);
-                                    let mut byte_string = byte_string.skip(12);
-                                    let bid_price_1 = consume_and_spit(&mut byte_string, 5);
-                                    let bid_qty_1 = consume_and_spit(&mut byte_string, 7);
-                                    let bid_price_2 = consume_and_spit(&mut byte_string, 5);
-                                    let bid_qty_2 = consume_and_spit(&mut byte_string, 7);
-                                    let bid_price_3 = consume_and_spit(&mut byte_string, 5);
-                                    let bid_qty_3 = consume_and_spit(&mut byte_string, 7);
-                                    let bid_price_4 = consume_and_spit(&mut byte_string, 5);
-                                    let bid_qty_4 = consume_and_spit(&mut byte_string, 7);
-                                    let bid_price_5 = consume_and_spit(&mut byte_string, 5);
-                                    let bid_qty_5 = consume_and_spit(&mut byte_string, 7);
-                                    let mut byte_string = byte_string.skip(7);
-                                    let ask_price_1 = consume_and_spit(&mut byte_string, 5);
-                                    let ask_qty_1 = consume_and_spit(&mut byte_string, 7);
-                                    let ask_price_2 = consume_and_spit(&mut byte_string, 5);
-                                    let ask_qty_2 = consume_and_spit(&mut byte_string, 7);
-                                    let ask_price_3 = consume_and_spit(&mut byte_string, 5);
-                                    let ask_qty_3 = consume_and_spit(&mut byte_string, 7);
-                                    let ask_price_4 = consume_and_spit(&mut byte_string, 5);
-                                    let ask_qty_4 = consume_and_spit(&mut byte_string, 7);
-                                    let ask_price_5 = consume_and_spit(&mut byte_string, 5);
-                                    let ask_qty_5 = consume_and_spit(&mut byte_string, 7);
-                                    let mut byte_string = byte_string.skip(50);
-                                    let accept_time = consume_and_spit(&mut byte_string, 8);
+                                    let issue_code = &arr[0];
+                                    let bid_price_1 = &arr[1].parse::<f32>().unwrap() / 100.0;
+                                    let bid_qty_1 = &arr[2].parse::<f32>().unwrap() / 100.0;
+                                    let bid_price_2 = &arr[3].parse::<f32>().unwrap() / 100.0;
+                                    let bid_qty_2 = &arr[4].parse::<f32>().unwrap() / 100.0;
+                                    let bid_price_3 = &arr[5].parse::<f32>().unwrap() / 100.0;
+                                    let bid_qty_3 = &arr[6].parse::<f32>().unwrap() / 100.0;
+                                    let bid_price_4 = &arr[7].parse::<f32>().unwrap() / 100.0;
+                                    let bid_qty_4 = &arr[8].parse::<f32>().unwrap() / 100.0;
+                                    let bid_price_5 = &arr[9].parse::<f32>().unwrap() / 100.0;
+                                    let bid_qty_5 = &arr[10].parse::<f32>().unwrap() / 100.0;
+                                    let ask_price_1 = &arr[11].parse::<f32>().unwrap() / 100.0;
+                                    let ask_qty_1 = &arr[12].parse::<f32>().unwrap() / 100.0;
+                                    let ask_price_2 = &arr[13].parse::<f32>().unwrap() / 100.0;
+                                    let ask_qty_2 = &arr[14].parse::<f32>().unwrap() / 100.0;
+                                    let ask_price_3 = &arr[15].parse::<f32>().unwrap() / 100.0;
+                                    let ask_qty_3 = &arr[16].parse::<f32>().unwrap() / 100.0;
+                                    let ask_price_4 = &arr[17].parse::<f32>().unwrap() / 100.0;
+                                    let ask_qty_4 = &arr[18].parse::<f32>().unwrap() / 100.0;
+                                    let ask_price_5 = &arr[19].parse::<f32>().unwrap() / 100.0;
+                                    let ask_qty_5 = &arr[20].parse::<f32>().unwrap() / 100.0;
+                                    let accept_time = &arr[21];
                                     println!("{} {} {} {}@{} {}@{} {}@{} {}@{} {}@{} {}@{} {}@{} {}@{} {}@{} {}@{}", pkt_time, accept_time, issue_code, bid_qty_5, bid_price_5, bid_qty_4, bid_price_4, bid_qty_3, bid_price_3, bid_qty_2, bid_price_2, bid_qty_1, bid_price_1, ask_qty_1, ask_price_1, ask_qty_2, ask_price_2, ask_qty_3, ask_price_3, ask_qty_4, ask_price_4, ask_qty_5, ask_price_5);
                                 }
                                 Ok(_) => (),
@@ -106,12 +109,26 @@ struct Quote<'a> {
     ask_price_5: u8,
 }
 
-fn consume_and_spit(it: &mut Iterator<Item = char>, num: u32) -> String {
-    let mut count = 0;
-    let mut out: String = "".into();
-    while count < num {
-        out.push(it.next().unwrap());
-        count += 1;
+fn consume(it: &mut Iterator<Item = char>, slices: Vec<i32>) -> Vec<String> {
+    let mut out: Vec<String> = vec![];
+    // this is N^2 time complexity...
+    for n in slices {
+        if n < 0 {
+            let skip = n * -1;
+            let mut count = 0;
+            while count < skip {
+                it.next();
+                count += 1;
+            }
+        } else {
+            let mut count = 0;
+            let mut el: String = "".into();
+            while count < n {
+                el.push(it.next().unwrap());
+                count += 1;
+            }
+            out.push(el);
+        }
     }
     out
 }
